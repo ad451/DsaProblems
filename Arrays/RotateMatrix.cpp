6a0525897ee2cd44d7e120410fb6424d5f58455b
ad451 <71942892+ad451@users.noreply.github.com>
2023-06-19 15:16:03 +0530

    Added folders for different types of problems

---
+++ Arrays/RotateMatrix.cpp
@@ -0,0 +1,62 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+
+     int n= matrix.size();
+    
+    //taking the transpose
+     for(int i=0;i<n;i++){
+         for (int j=0;j<n-i-1;j++){
+             swap(matrix[i][j],matrix[n-1-j][n-i-1]);
+         }
+     }
+
+
+     //Reversing the order of the numbers by column
+
+     
+
+    
+
+
+    }
+};
+
+int main(){
+    //creating a Solution class object
+    Solution* solution = new Solution();
+
+    //declaring the matrix
+    vector<vector<int>> Matrix;
+    int size;
+    cin>>size;
+
+    //Taking the input for the Matrix
+    for(int i=0;i<size;i++){
+        vector<int> temp;
+        for(int j=0;j<size;j++){
+            int number;
+            cin>>number;
+            temp.push_back(number);
+
+        }
+        Matrix.push_back(temp);
+    }
+
+    //calling the rotate method of the solution object
+    solution->rotate(Matrix);
+
+
+    //printing the matrix after the rotate method was called 
+    for(auto x:Matrix){
+        for(auto y:x){
+            cout<<y<<" ";
+        }
+        cout<<endl;
+    }
+
+
+}
